0cda80e8723422b9f127070a582bdf1399e1165f
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2026-01-18 21:41:09 -0500

    tell flask it is behind a proxy

--- discotron.py
+++ discotron.py
@@ -3,6 +3,7 @@ import requests
 
 from flask import Flask, session, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.middleware.proxy_fix import ProxyFix
 from authlib.integrations.flask_client import OAuth
 
 from dotenv import load_dotenv
@@ -21,6 +22,7 @@ project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = "sqlite:///{}".format(os.path.join(project_dir, "discotron.db"))
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.secret_key = get_env_or_secret("SECRET_KEY")
